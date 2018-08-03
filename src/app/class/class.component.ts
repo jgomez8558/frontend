183e4f42a2bc677160176e4d6f19b797c2f887fa
Unknown <seanfoytek@gmail.com>
2018-08-03 16:07:13 -0500

    Update
    Need to add carosoual

--- src/app/class/class.component.ts
+++ src/app/class/class.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+
+
 
 @Component({
   selector: 'app-class',
   templateUrl: './class.component.html',
   styleUrls: ['./class.component.css']
+  
 })
 export class ClassComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
-  }
-
-}
+ public ngOnInit()  {
+  
+ }}
